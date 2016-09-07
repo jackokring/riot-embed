d57fcec80de119ca4d4e07dc5794bbd943b2a3ee
Simon Jackson <jackokring@gmail.com>
2016-09-07 20:52:53 +0100

    And the starts of queries for ajaxy likey stuff.

---
+++ dbase.php
@@ -0,0 +1,15 @@
+<?php
+include('../wp-config.php');
+//Now we have DB_NAME, DB_USER, DB_PASSWORD, DB_HOST for making a show!
+
+define('DB_PREFIX', $table_prefix . $table_prefix);
+//This should avoid some problems, but not all!
+
+/* The install used for development uses /testy/dbase.php as a default ModRewrite
+	to this script. As this is a proxy [P] to /var/www/html/dbase.php then
+	../wp-config.php is one up in /var/www. Make sure you correct the first
+	include line for this. IMPORTANT!! */
+	
+
+
+?>
