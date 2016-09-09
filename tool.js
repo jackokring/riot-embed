72ae96559c356f2a63e495885c8f27b39a419870
Simon Jackson <jackokring@gmail.com>
2016-09-09 19:54:05 +0100

    OK

--- tool.js
+++ tool.js
@@ -86,9 +86,9 @@ function savePON(json, callback, load) {
     http.setRequestHeader("Content-type", "application/json");
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
-            var tr = http.responseText;
-            tr.id = id;
-            callback && callback(unpack(tr));
+            var tr = unpack(http.responseText);
+            tr.id.__ = id;
+            callback && callback(tr);
         }
     }
     var pj = pack(json);
