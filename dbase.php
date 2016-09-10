1f2c18134a8c557d8a37f76f06ead79ce7f82b36
Simon Jackson <jackokring@gmail.com>
2016-09-10 21:50:46 +0100

    Update dbase.php

--- dbase.php
+++ dbase.php
@@ -1,14 +1,12 @@
 <?php
-include('../wp-config.php');
+//================================================
+// DROP THIS FILE IN YOUR WORDPRESS FOLDER
+//================================================
+include('wp-config.php');
 //Now we have DB_NAME, DB_USER, DB_PASSWORD, DB_HOST for making a show!
 
 define('DB_PREFIX', 're-');
 //This should avoid some problems!
-
-/* The install used for development uses /testy/dbase.php as a default ModRewrite
-	to this script. As this is a proxy [P] to /var/www/html/dbase.php then
-	../wp-config.php is one up in /var/www. Make sure you correct the first
-	include line for this. IMPORTANT!! */
 	
 header('Content-Type: application/json');
 //set a general javascript return type. This can auto encapsulate JSON.
