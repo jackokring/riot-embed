15bcd1cf3c4c876ec20f18aee0be038aa6b70396
Simon Jackson <jackokring@gmail.com>
2016-09-10 21:07:37 +0100

    Create test.tool.js

---
+++ test.tool.js
@@ -0,0 +1,8 @@
+//      RiotEmbed tool.js 0.0.0
+//      https://kring.co.uk
+//      (c) 2016 Simon Jackson, K Ring Technologies Ltd
+//      MIT, like as he said. And underscored :D
+
+//==========================================
+// TEST SUITE FOR tool.js AND ...
+//==========================================
