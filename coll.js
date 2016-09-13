43db1bf018789f9607f9e5f0ee51eab5193b1e70
Simon Jackson <jackokring@gmail.com>
2016-09-13 06:16:54 +0100

    Update coll.js

--- coll.js
+++ coll.js
@@ -3,5 +3,80 @@
 //========================================
 
 function _$(obj) {
+  
+  function concat() {
+    
+  }
 
+  function join() {
+    
+  }
+  
+  function push() {
+    
+  }
+  
+  function pop() {
+    
+  }
+  
+  function shift() {
+    
+  }
+  
+  function unshift() {
+    
+  }
+  
+  function slice() {
+    
+  }
+  
+  function splice() {
+    
+  }
+  
+  function reverse() {
+    
+  }
+  
+  function sort() {
+    
+  }
+  
+  function indexOf() {
+    
+  }
+  
+  function lastIndexOf() {
+    
+  }
+  
+  function forEach() {
+    
+  }
+  
+  function map() {
+    
+  }
+  
+  function filter() {
+    
+  }
+  
+  function every() {
+    
+  }
+  
+  function some() {
+    
+  }
+  
+  function reduce() {
+    
+  }
+  
+  function reduceRight() {
+    
+  }
 }
