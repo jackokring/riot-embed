117ef8c1ad8848787218cc142db61e6db802c166
Simon Jackson <jackokring@gmail.com>
2016-09-14 02:10:52 +0100

    Update coll.js

--- coll.js
+++ coll.js
@@ -4,26 +4,40 @@
 
 function _$(obj) {
   
-  function concat() {
-    
+  this._idx = new Array();
+  var inner = this;
+  _.each(obj, function(el) {
+    inner.push(el);
+  });
+  return this;
+  
+  function concat(all) {
+    var res = new _$(this);
+    _.each(all, function(el) {
+      res.push(el);
+    });
   }
 
   /* function join() */
   
-  function push() {
-    
+  function push(el) {
+    this._idx.push(el._i);//becomes finable
+    return super.push(el);
   }
   
   function pop() {
-    
+    this._idx.pop();//make not found
+    return super.pop();
   }
   
-  function shift() {
-    
+  function shift() {//synchronized auto
+    this._idx.shift();
+    return super.shift();
   }
   
-  function unshift() {
-    
+  function unshift(el) {
+    this._idx.unshift(el._i);
+    return super.unshift(el);
   }
   
   function slice() {
@@ -35,7 +49,8 @@ function _$(obj) {
   }
   
   function reverse() {
-    
+    this._idx.reverse();
+    return this.reverse();
   }
   
   function sort() {
